c614c98d71f090d0616df66869fbc9460410f5fe
kmanjt <kevin.tomescu2@mail.dcu.ie>
2023-03-29 15:40:10 +0100

    Setup Django+React integration, env.example, reqs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
+from django.shortcuts import render
 
+def render_react(request):
+    return render(request, "index.html")
+  
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path("admin/", admin.site.urls),
+  re_path(r"^$", render_react),
+  re_path(r"^(?:.*)/?$", render_react),
 ]
